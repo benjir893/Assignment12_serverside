0a7740f47e67264df3075ac64f190e43c6d41e90
benjir bhuyan <benjir_bhuyan@yahoo.com>
2024-12-25 12:47:48 +0800

    connected database collection

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-require('dotenv').config
+require('dotenv').config()
 const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 const app = express();
-const port = process.env.PORT||8000
+const port = process.env.PORT||5000
 
 // middleware
 app.use(express.json())
@@ -27,6 +27,12 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
+    const servicesCollection = client.db('diagnosticcenter').collection('services')
+
+    app.get('/services', async(req, res)=>{
+      const result = await servicesCollection.find().toArray();
+      res.send(result)
+    })
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
